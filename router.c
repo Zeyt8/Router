40dce0dbeea05873078676f1e17dfcba9eb783c8
BogdanFoca <92569782+BogdanFoca@users.noreply.github.com>
2022-04-04 06:32:30 -0700

    Moved functions back to router from skel

--- router.c
+++ router.c
@@ -44,6 +44,75 @@ struct arp_entry* checkIfIPv4ExistsInARP(__u32 ip);
  */
 struct ether_header* createEthernetHeader(uint8_t *sha, uint8_t *dha, unsigned short type);
 
+/**
+ * @brief 
+ * 
+ * @param daddr destination IP
+ * @param saddr source IP
+ * @param sha source MAC
+ * @param dha destination MAC
+ * @param type Type
+ * @param code Code
+ * @param interface interface 
+ * @param id id
+ * @param seq sequence
+ */
+void send_icmp(uint32_t daddr, uint32_t saddr, uint8_t *sha, uint8_t *dha, u_int8_t type, u_int8_t code, int interface, int id, int seq);
+
+
+/**
+ * @brief 
+ * 
+ * @param daddr destination IP
+ * @param saddr source IP
+ * @param sha source MAC
+ * @param dha destination MAC
+ * @param type Type
+ * @param code Code
+ * @param interface interface 
+ */
+void send_icmp_error(uint32_t daddr, uint32_t saddr, uint8_t *sha, uint8_t *dha, u_int8_t type, u_int8_t code, int interface);
+
+
+/**
+ * @brief 
+ * 
+ * @param daddr destination IP address
+ * @param saddr source IP address
+ * @param eth_hdr ethernet header
+ * @param interface interface
+ * @param arp_op ARP OP: ARPOP_REQUEST or ARPOP_REPLY
+ */
+void send_arp(uint32_t daddr, uint32_t saddr, struct ether_header *eth_hdr, int interface, uint16_t arp_op);
+
+/**
+ * @brief 
+ * 
+ * @param buffer 
+ * @return struct arp_header* A pointer to a structure of type arp_header that is inside the buffer. Basically, we return the
+ * arp header from buffer.
+ * If this is not an ARP frame, we return NULL.
+ */
+struct arp_header* parse_arp(void *buffer);
+
+/**
+ * @brief 
+ * 
+ * @param buffer 
+ * @return struct icmphdr* A pointer to a structure of type icmphdr that is inside the buffer. Basically, we return the
+ * icmp header from buffer.
+ * If this is not an ICMP packet, we return NULL.
+ */
+struct icmphdr * parse_icmp(void *buffer);
+
+/**
+ * hwaddr_aton - Convert ASCII string to MAC address (colon-delimited format)
+ * @txt: MAC address as a string (e.g., "00:11:22:33:44:55")
+ * @addr: Buffer for the MAC address (ETH_ALEN = 6 bytes)
+ * Returns: 0 on success, -1 on failure (e.g., string not a MAC address)
+ */
+int hwaddr_aton(const char *txt, uint8_t *addr);
+
 int main(int argc, char *argv[])
 {
 	packet m;
@@ -78,7 +147,7 @@ int main(int argc, char *argv[])
 			bool success = handleICMP(m, icmp_hdr, ip_hdr, ethernet_hdr);
 			if(!success)
 			{
-				continue;
+				continue;	//Drop the package
 			}
 		}
 		else
@@ -111,7 +180,9 @@ bool handleARP(packet m, struct arp_header* arp_hdr, struct ether_header* ethern
 		uint8_t mac = malloc(sizeof(6));
 		get_interface_mac(m.interface, mac);
 		struct ether_header* e_h = createEthernetHeader(mac, ethernet_hdr->ether_shost, ethernet_hdr->ether_type);
-		end_arp(arp_hdr->spa, arp_hdr->tpa, e_h, m.interface, htons(2));
+		send_arp(arp_hdr->spa, arp_hdr->tpa, e_h, m.interface, htons(2));
+		free(e_h);
+		free(mac);
 	}
 	else if(ntohs(arp_hdr->op) == 1){
 		return false;	//Drop the package
@@ -126,6 +197,32 @@ bool handleARP(packet m, struct arp_header* arp_hdr, struct ether_header* ethern
 		if(!queue_empty(packageQueue))
 		{
 			packet* pack = queue_deq(packageQueue);
+			struct ether_header* p_eth_hdr = malloc(sizeof(struct ether_header));
+			struct iphdr* p_ip_hdr = malloc(sizeof(struct iphdr));
+
+			if(p_ip_hdr->ttl <= 1)
+			{
+				//TODO send ttl error
+				return false;	//Drop the packet
+			}
+
+			__u16 check = p_ip_hdr->check;
+			p_ip_hdr->check = ip_checksum(p_ip_hdr, sizeof(struct iphdr));
+
+			if(check != p_ip_hdr)
+			{
+				//TODO sens checksum error
+				return false;	//Drop the packet
+			}
+
+			p_ip_hdr->ttl--;
+
+			free(p_eth_hdr);
+			free(p_ip_hdr);
+		}
+		else
+		{
+			return false;	//Drop the packet
 		}
 	}
 }
@@ -159,6 +256,149 @@ struct arp_entry* checkIfIPv4ExistsInARP(__u32 ip)
 struct ether_header* createEthernetHeader(uint8_t *sha, uint8_t *dha, unsigned short type)
 {
 	struct ether_header *eth_hdr = malloc(sizeof(struct ether_header));
-	build_ethhdr(eth_hdr, sha, dha, type);
+	memcpy(eth_hdr->ether_dhost, dha, ETH_ALEN);
+	memcpy(eth_hdr->ether_shost, sha, ETH_ALEN);
+	eth_hdr->ether_type = type;
 	return eth_hdr;
+}
+
+void send_icmp(uint32_t daddr, uint32_t saddr, uint8_t *sha, uint8_t *dha, u_int8_t type, u_int8_t code, int interface, int id, int seq)
+{
+
+	struct ether_header eth_hdr;
+	struct iphdr ip_hdr;
+	struct icmphdr icmp_hdr = {
+		.type = type,
+		.code = code,
+		.checksum = 0,
+		.un.echo = {
+			.id = id,
+			.sequence = seq,
+		}
+	};
+	packet packet;
+	void *payload;
+
+	build_ethhdr(&eth_hdr, sha, dha, htons(ETHERTYPE_IP));
+	/* No options */
+	ip_hdr.version = 4;
+	ip_hdr.ihl = 5;
+	ip_hdr.tos = 0;
+	ip_hdr.protocol = IPPROTO_ICMP;
+	ip_hdr.tot_len = htons(sizeof(struct iphdr) + sizeof(struct icmphdr));
+	ip_hdr.id = htons(1);
+	ip_hdr.frag_off = 0;
+	ip_hdr.ttl = 64;
+	ip_hdr.check = 0;
+	ip_hdr.daddr = daddr;
+	ip_hdr.saddr = saddr;
+	ip_hdr.check = ip_checksum(&ip_hdr, sizeof(struct iphdr));
+	
+	icmp_hdr.checksum = icmp_checksum((uint16_t *)&icmp_hdr, sizeof(struct icmphdr));
+
+	payload = packet.payload;
+	memcpy(payload, &eth_hdr, sizeof(struct ether_header));
+	payload += sizeof(struct ether_header);
+	memcpy(payload, &ip_hdr, sizeof(struct iphdr));
+	payload += sizeof(struct iphdr);
+	memcpy(payload, &icmp_hdr, sizeof(struct icmphdr));
+	packet.len = sizeof(struct ether_header) + sizeof(struct iphdr) + sizeof(struct icmphdr);
+
+	send_packet(&packet);
+}
+
+void send_icmp_error(uint32_t daddr, uint32_t saddr, uint8_t *sha, uint8_t *dha, u_int8_t type, u_int8_t code, int interface)
+{
+
+	struct ether_header eth_hdr;
+	struct iphdr ip_hdr;
+	struct icmphdr icmp_hdr = {
+		.type = type,
+		.code = code,
+		.checksum = 0,
+	};
+	packet packet;
+	void *payload;
+
+	build_ethhdr(&eth_hdr, sha, dha, htons(ETHERTYPE_IP));
+	/* No options */
+	ip_hdr.version = 4;
+	ip_hdr.ihl = 5;
+	ip_hdr.tos = 0;
+	ip_hdr.protocol = IPPROTO_ICMP;
+	ip_hdr.tot_len = htons(sizeof(struct iphdr) + sizeof(struct icmphdr));
+	ip_hdr.id = htons(1);
+	ip_hdr.frag_off = 0;
+	ip_hdr.ttl = 64;
+	ip_hdr.check = 0;
+	ip_hdr.daddr = daddr;
+	ip_hdr.saddr = saddr;
+	ip_hdr.check = ip_checksum(&ip_hdr, sizeof(struct iphdr));
+	
+	icmp_hdr.checksum = icmp_checksum((uint16_t *)&icmp_hdr, sizeof(struct icmphdr));
+
+	payload = packet.payload;
+	memcpy(payload, &eth_hdr, sizeof(struct ether_header));
+	payload += sizeof(struct ether_header);
+	memcpy(payload, &ip_hdr, sizeof(struct iphdr));
+	payload += sizeof(struct iphdr);
+	memcpy(payload, &icmp_hdr, sizeof(struct icmphdr));
+	packet.len = sizeof(struct ether_header) + sizeof(struct iphdr) + sizeof(struct icmphdr);
+
+	send_packet(&packet);
+}
+
+void send_arp(uint32_t daddr, uint32_t saddr, struct ether_header *eth_hdr, int interface, uint16_t arp_op)
+{
+	struct arp_header arp_hdr;
+	packet packet;
+
+	arp_hdr.htype = htons(ARPHRD_ETHER);
+	arp_hdr.ptype = htons(2048);
+	arp_hdr.op = arp_op;
+	arp_hdr.hlen = 6;
+	arp_hdr.plen = 4;
+	memcpy(arp_hdr.sha, eth_hdr->ether_shost, 6);
+	memcpy(arp_hdr.tha, eth_hdr->ether_dhost, 6);
+	arp_hdr.spa = saddr;
+	arp_hdr.tpa = daddr;
+	memset(packet.payload, 0, 1600);
+	memcpy(packet.payload, eth_hdr, sizeof(struct ethhdr));
+	memcpy(packet.payload + sizeof(struct ethhdr), &arp_hdr, sizeof(struct arp_header));
+	packet.len = sizeof(struct arp_header) + sizeof(struct ethhdr);
+	send_packet(&packet);
+}
+
+struct arp_header* parse_arp(void *buffer)
+{
+	struct arp_header *arp_hdr;
+	struct ether_header *eth_hdr;
+
+	eth_hdr = (struct ether_header *)buffer;
+	if (ntohs(eth_hdr->ether_type) == ETHERTYPE_ARP) {
+		arp_hdr = (struct arp_header *)(buffer + sizeof(struct ether_header));
+		return arp_hdr;
+	} else
+		return NULL;
+
+}
+
+struct icmphdr * parse_icmp(void *buffer)
+{
+	struct ether_header *eth_hdr;
+	struct iphdr *ip_hdr;
+
+	eth_hdr = (struct ether_header *)buffer;
+	if (ntohs(eth_hdr->ether_type) == ETHERTYPE_IP) {
+		ip_hdr = (struct iphdr *)(buffer + sizeof(struct ether_header));
+			if (ip_hdr->protocol == 1) {
+				struct icmphdr *icmp_hdr;
+				icmp_hdr = (struct icmphdr *)(buffer + sizeof(struct iphdr) + sizeof(struct ether_header));
+				return icmp_hdr;
+			} else
+				return NULL;
+
+
+	} else
+		return NULL;
 }
